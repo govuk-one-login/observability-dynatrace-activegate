81d57a4c8db98aafd500e83e09179e68a14dc9b4
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-09-28 14:11:05 +0100

    Debug for ASG

--- lib/dynatrace-activegate-stack.ts
+++ lib/dynatrace-activegate-stack.ts
@@ -12,7 +12,7 @@ export class DynatraceActivegateStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'vpc', {
-      maxAzs: 1
+      maxAzs: 3
     });
 
     const userData = ec2.UserData.forLinux();
