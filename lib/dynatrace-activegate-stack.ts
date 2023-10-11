3d22dba2302c207adba74cadeb3a81ddb4f68c51
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-11 16:18:17 +0100

    Added in a symlink so all users have access to the new version of curl and added the -y flag so packages actually install

--- lib/dynatrace-activegate-stack.ts
+++ lib/dynatrace-activegate-stack.ts
@@ -18,13 +18,14 @@ export class DynatraceActivegateStack extends cdk.Stack {
     const userData = ec2.UserData.forLinux();
 
     userData.addCommands(
-      'yum update',
-      'yum install gcc openssl openssl-devel',
+      'yum update -y',
+      'yum install gcc openssl openssl-devel -y',
       'wget https://curl.se/download/curl-8.4.0.tar.gz',
       'tar -xvf curl-8.4.0.tar.gz curl-8.4.0',
       'cd curl-8.4.0',
       'sed -i \'s/for ac_option in --version -v -V -qversion -version; do/for ac_option in --version -v -qversion -version; do/g\' ./configure',
       './configure --with-openssl; make && make install',
+      'mv /usr/bin/curl /usr/bin/curl_back; ln -s /usr/local/bin/curl /usr/bin/curl',
       'cd -',
       'export DT_TOKEN=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id dynatrace-token --query SecretString --output text)',
       'export DT_URL=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id dynatrace-url --query SecretString --output text)',
